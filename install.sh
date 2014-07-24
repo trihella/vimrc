0ca540de11f99b9b53c279b9d390c7126c2033f0
#6 <trihella@gmail.com>
2014-07-24 16:01:32 +0000

    Install script missing directory creation
    In the case where ~/.vim directory did not exists, the installation
    failed. Fixed by always creating directory .vim/bundle

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd`
+mkdir -p $SCRIPT_PATH/vim/bundle
 
 # Creating symbolik links
 ln -sf $SCRIPT_PATH/vimrc ~/.vimrc
@@ -10,7 +11,6 @@ ln -sf $SCRIPT_PATH/vimrc.bundles ~/.vimrc.bundles
 ln -sf $SCRIPT_PATH/vimrc.bepo ~/.vimrc.bepo
 ln -sf $SCRIPT_PATH/vim ~/.vim
 
-#mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim -u $SCRIPT_PATH/vimrc.install +PluginInstall
